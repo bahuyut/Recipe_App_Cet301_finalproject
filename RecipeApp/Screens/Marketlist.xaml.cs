aedb8d2834cb14e554a8f04f9783e1d0d0b3a261
bahuyut <bayramalihuyut99@gmail.com>
2024-01-16 00:57:47 +0300

    Market listesi boşken ekrana liste boş uyarısı eklendi

--- RecipeApp/Screens/Marketlist.xaml.cs
+++ RecipeApp/Screens/Marketlist.xaml.cs
@@ -1,56 +1,59 @@
-﻿
+﻿using RecipeApp.Screens;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-
-using RecipeApp.Screens;
-
-namespace RecipeApp.Screens;
-
-public partial class Marketlist : ContentPage
+namespace RecipeApp.Screens
 {
-    MarketlistRepository marketlistRepository;
-    public Marketlist()
+    public partial class Marketlist : ContentPage
     {
-        InitializeComponent();
-        marketlistRepository = new MarketlistRepository();
-    }
+        private MarketlistRepository marketlistRepository;
 
-    protected override async void OnAppearing()
-    {
-        base.OnAppearing();
-        var item = await marketlistRepository.GetAllItems();
-        marketListView.ItemsSource = item;
-
-    }
-
-
-    private async void OnAddClicked(object sender, EventArgs e)
-    {
-        
-        
-
-        MarketItem item = new MarketItem
+        public Marketlist()
         {
-            Name = itemEntry.Text,
-            
-        };
-        await marketlistRepository.AddItem(item);
-        var updatedItems = await marketlistRepository.GetAllItems();
-            marketListView.ItemsSource = updatedItems;
-    }
+            InitializeComponent();
+            marketlistRepository = new MarketlistRepository();
+        }
 
-    private async void OnDeleteClicked(object sender, EventArgs e)
-    {
-        Button btn = (Button)sender;
-        MarketItem itemToDelete = (MarketItem)btn.CommandParameter;
-
-        bool answer = await DisplayAlert("Uyarı", "Silmek istediğinize emin misiniz?", "Evet", "Hayır");
-
-        if (answer)
+        protected override async void OnAppearing()
         {
-            await marketlistRepository.DeleteItem(itemToDelete);
-            var updatedItems = await marketlistRepository.GetAllItems();
-            marketListView.ItemsSource = updatedItems;
+            base.OnAppearing();
+            await UpdateMarketList();
+        }
+
+        private async Task UpdateMarketList()
+        {
+            var items = await marketlistRepository.GetAllItems();
+            marketListView.ItemsSource = items;
+
+            // Show/hide the empty list label based on the market list's emptiness
+            emptyListLabel.IsVisible = items.Count == 0;
+        }
+
+        private async void OnAddClicked(object sender, EventArgs e)
+        {
+            MarketItem item = new MarketItem
+            {
+                Name = itemEntry.Text,
+            };
+
+            await marketlistRepository.AddItem(item);
+            itemEntry.Text = string.Empty; // Clear the entry after adding an item
+            await UpdateMarketList();
+        }
+
+        private async void OnDeleteClicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            MarketItem itemToDelete = (MarketItem)btn.CommandParameter;
+
+            bool answer = await DisplayAlert("Uyarı", "Silmek istediğinize emin misiniz?", "Evet", "Hayır");
+
+            if (answer)
+            {
+                await marketlistRepository.DeleteItem(itemToDelete);
+                await UpdateMarketList();
+            }
         }
     }
-
 }
